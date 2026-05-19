eaf5ca05c71f2216b89e9e63ed73db404ad796c6
Lennebraha38 <farukhilmi12345@gmail.com>
2026-05-19 23:41:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,13 @@ from werkzeug.security import generate_password_hash, check_password_hash
 app = Flask(__name__, 
             template_folder='templates',
             static_folder='static')
+# Veritabanının silinmesini engellemek için kalıcı bir dosya yolu oluşturur
+base_dir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(base_dir, 'veritabani.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'teknofest_gizli_anahtar_2026' # Güvenli oturumlar için
+
+db = SQLAlchemy(app)
 
 @app.route('/')
 def home():
